363c1c6d0deb0f81afd881aec67bcfe510115847
Chemthunder <chemthunderisme@gmail.com>
2026-06-10 14:36:47 -0400

    d

--- building.ts
+++ building.ts
@@ -1,75 +1,54 @@
-class Vec2 {
-    private x: number;
-    private y: number;
+const i = img`
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+`;
 
-    public constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
+const Test = new SpriteBuilder()
+    .withImage(i)
+    .withKind(SpriteKind.Placeholder)
+    .withFlag(SpriteFlag.StayInScreen)
+    .withPosition(screen.width / 2, screen.height / 2, 90)
+    .build();
+
+forever(() => {
+    const range = 150;
+
+    if (randint(1, 9) > 6) {
+        Test.setVelocity(
+            randint(-range, range),
+            randint(-range, range)
+        );
+    }
+});
+
+let fac = new particles.ShapeFactory(15, 15, createImage(16, 16, game.Color.Purple));
+let src = new particles.ParticleSource(Test, 50, fac);
+let par = fac.createParticle(src.anchor);
+
+fac.setSpeed(0);
+
+for (let i = 16; i > 0; i -= 2) {
+    let color = 0;
+
+    if (randint(1, 2) == 2) {
+        color = game.Color.Purple
+    } else {
+        color = 1;
     }
 
-    public getX(): number {
-        return this.x;
-    }
-
-    public getY(): number {
-        return this.y;
-    }
-
-    public setX(x: number) {
-        this.x = x;
-    }
-
-    public setY(y: number) {
-        this.y = y;
-    }
-}
-
-class Vec3 {
-    private x: number;
-    private y: number;
-    private z: number;
-
-    public constructor(x: number, y: number, z: number) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
-    }
-
-    public getX(): number {
-        return this.x;
-    }
-
-    public getY(): number {
-        return this.y;
-    }
-
-    public getZ(): number {
-        return this.z;
-    }
-
-    public setX(x: number) {
-        this.x = x;
-    }
-
-    public setY(y: number) {
-        this.y = y;
-    }
-
-    public setZ(z: number) {
-        this.z = z;
-    }
-}
-
-function input(btn: controller.Button, handler: () => void) {
-    btn.onEvent(ControllerButtonEvent.Pressed, handler);
-}
-
-function tile(color?: number): Image {
-    return createImage(16, 16, color != null ? color : game.Color.Transparent);
-}
-
-namespace tiles {
-    export function getCurrentTilemap(): TileMap {
-        return Dispatcher.fetchScene().tileMap;
-    }
+    fac.addShape(createImage(i, i, color));
 }
--- coil.ts
+++ coil.ts
@@ -101,6 +101,69 @@ class Scene {
     }
 }
 
+// MATH
+class Vec2 {
+    private x: number;
+    private y: number;
+
+    public constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public getX(): number {
+        return this.x;
+    }
+
+    public getY(): number {
+        return this.y;
+    }
+
+    public setX(x: number) {
+        this.x = x;
+    }
+
+    public setY(y: number) {
+        this.y = y;
+    }
+}
+
+class Vec3 {
+    private x: number;
+    private y: number;
+    private z: number;
+
+    public constructor(x: number, y: number, z: number) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public getX(): number {
+        return this.x;
+    }
+
+    public getY(): number {
+        return this.y;
+    }
+
+    public getZ(): number {
+        return this.z;
+    }
+
+    public setX(x: number) {
+        this.x = x;
+    }
+
+    public setY(y: number) {
+        this.y = y;
+    }
+
+    public setZ(z: number) {
+        this.z = z;
+    }
+}
+
 // BUILDING
 class GameMetaData {
     private author: string;
@@ -891,5 +954,19 @@ namespace Util {
         } else {
             return list[index += 1];
         }
+    }
+}
+
+function input(btn: controller.Button, handler: () => void) {
+    btn.onEvent(ControllerButtonEvent.Pressed, handler);
+}
+
+function tile(color?: number): Image {
+    return createImage(16, 16, color != null ? color : game.Color.Transparent);
+}
+
+namespace tiles {
+    export function getCurrentTilemap(): TileMap {
+        return Dispatcher.fetchScene().tileMap;
     }
 }
